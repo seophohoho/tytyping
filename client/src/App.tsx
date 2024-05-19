e722258e4fce16b392a00d4595329fa883c98290
uyeon0209 <your_email@example.com>
2024-05-20 03:05:18 +0900

    [Design]: Game page structure design 게임 페이지 전체 디자인 완료

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from 'react-router-dom';
 import Main from './component/Main';
 import MatchingReady from './component/MatchingReady';
+import Game from './component/Game';
 
 function App() {
   return (
     <Routes>
       <Route path="/main" element={<Main />} />
       <Route path="/matching-ready" element={<MatchingReady />} />
+      <Route path="/game" element={<Game />} />
     </Routes>
   );
 }

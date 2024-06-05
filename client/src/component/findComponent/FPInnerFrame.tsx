cc7211fda40f579cf78a252d31cd551d0ce18d71
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:23:30 +0900

    Update FPInnerFrame.tsx
    Modification due to changes in file directory structure

--- client/src/component/findComponent/FPInnerFrame.tsx
+++ client/src/component/findComponent/FPInnerFrame.tsx
@@ -1,15 +1,31 @@
 import React from 'react';
-import styles from '../../styles/FindInnerFrame.module.css';
+import styles from '../styles/FindInnerFrame.module.css';
 
 interface FPInnerFrameProps {
   username: string;
   email: string;
+  verificationCode: string;
   onUsernameChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   onEmailChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onSubmit: () => void;
+  onVerificationCodeChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onCheck: () => void;
+  onConfirm: () => void;
+  isVerificationSent: boolean;
+  isVerificationChecked: boolean;
 }
 
-function FPInnerFrame({ username, email, onUsernameChange, onEmailChange, onSubmit }: FPInnerFrameProps) {
+const FPInnerFrame: React.FC<FPInnerFrameProps> = ({
+  username,
+  email,
+  verificationCode,
+  onUsernameChange,
+  onEmailChange,
+  onVerificationCodeChange,
+  onCheck,
+  onConfirm,
+  isVerificationSent,
+  isVerificationChecked,
+}) => {
   return (
     <div className={styles.innerFrame}>
       <div className={styles.inputContainer}>
@@ -26,11 +42,30 @@ function FPInnerFrame({ username, email, onUsernameChange, onEmailChange, onSubm
         <div className={styles.labelBox}>Email</div>
         <input type="email" className={styles.emailInput} placeholder=" " value={email} onChange={onEmailChange} />
       </div>
-      <button type="button" className={styles.lineButton} onClick={onSubmit}>
-        OK
-      </button>
+      {!isVerificationChecked && (
+        <button className={styles.lineButton} onClick={onCheck}>
+          Check
+        </button>
+      )}
+      {isVerificationSent && (
+        <>
+          <div className={styles.inputContainer}>
+            <div className={styles.labelBox}>Code</div>
+            <input
+              type="text"
+              className={styles.verificationCodeInput}
+              placeholder=" "
+              value={verificationCode}
+              onChange={onVerificationCodeChange}
+            />
+          </div>
+          <button className={styles.confirmButton} onClick={onConfirm}>
+            Confirm
+          </button>
+        </>
+      )}
     </div>
   );
-}
+};
 
 export default FPInnerFrame;

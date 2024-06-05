61a77fcaa407f763b7a83d85da3a1183fa5593d6
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:24:05 +0900

    Update FUCenterFrame.tsx
    Modification due to changes in file directory structure

--- client/src/component/findComponent/FUCenterFrame.tsx
+++ client/src/component/findComponent/FUCenterFrame.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../../styles/FindCenterFrame.module.css';
+import styles from '../styles/FindCenterFrame.module.css';
 
-function FUCenterFrame() {
+const FUCenterFrame: React.FC = () => {
   return (
     <div className={styles.centerFrame}>
       <p className={styles.topText}>Forgot your username?</p>
-      <div className={styles.blackLine} />
-      <p className={styles.explainText}>Please enter your nickname && password</p>
+      <div className={styles.blackLine}></div>
+      <p className={styles.usernameexplainText}>Please enter your email</p>
       <Link to="/signin" className={styles.nonlineButton}>
         Sign In
       </Link>
     </div>
   );
-}
+};
 
 export default FUCenterFrame;

7d194e221ad6a5b635ef607bf24a7d131b651644
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:22:53 +0900

    Update FPCenterFrame.tsx
    Modification due to changes in file directory structure

--- client/src/component/findComponent/FPCenterFrame.tsx
+++ client/src/component/findComponent/FPCenterFrame.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../../styles/FindCenterFrame.module.css';
+import styles from '../styles/FindCenterFrame.module.css';
 
-function FPCenterFrame() {
+const FPCenterFrame: React.FC = () => {
   return (
     <div className={styles.centerFrame}>
       <p className={styles.topText}>Forgot your password?</p>
-      <div className={styles.blackLine} />
-      <p className={styles.explainText}>Please enter your username && email</p>
+      <div className={styles.blackLine}></div>
+      <p className={styles.passwordexplainText}>Please enter your username & email</p>
       <Link to="/signin" className={styles.nonlineButton}>
         Sign In
       </Link>
     </div>
   );
-}
+};
 
 export default FPCenterFrame;

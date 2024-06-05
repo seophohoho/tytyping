92a356858442444974677997d2057c53cf2ac422
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:41:39 +0900

    Update findUsernameRoute.ts
    Modification due to changes in file directory structure

--- server/src/routes/findUsernameRoute.ts
+++ server/src/routes/findUsernameRoute.ts
@@ -1,9 +1,9 @@
-import express from 'express';
-import { forgotUsernameController } from '../controllers/findUsernameController';
+import express from "express";
+import { forgotUsernameController } from "../controllers/findUsernameController";
 
 const router = express.Router();
 
 // 아이디 찾기 요청 처리
-router.post('/', forgotUsernameController);
+router.post("/", forgotUsernameController);
 
 export default router;

2b2cc92935c347fabe89fccc6ace26e6ed353003
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:25:47 +0900

    Update RPCenterFrame.tsx
    Modification due to changes in file directory structure

--- client/src/component/findComponent/RPCenterFrame.tsx
+++ client/src/component/findComponent/RPCenterFrame.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import styles from '../../styles/FindCenterFrame.module.css';
+import styles from '../styles/FindCenterFrame.module.css';
 
-function RPCenterFrame() {
+const RPCenterFrame: React.FC = () => {
   return (
     <div className={styles.centerFrame}>
       <p className={styles.topText}>Reset your password</p>
-      <div className={styles.blackLine} />
+      <div className={styles.blackLine}></div>
       <p className={styles.resetExplainText}>Please enter your new password</p>
     </div>
   );
-}
+};
 
 export default RPCenterFrame;

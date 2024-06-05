40ed20ea87b8b77ad64d2f9d3dbec7787ebf86c7
HAGI44 <155417945+HAGI44@users.noreply.github.com>
2024-06-05 23:29:49 +0900

    Update SUpCenterFrame.tsx
    Modification due to changes in file directory structure

--- client/src/component/signComponent/SUpCenterFrame.tsx
+++ client/src/component/signComponent/SUpCenterFrame.tsx
@@ -1,21 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import styles from '../../styles/CenterFrame.module.css';
+import styles from '../styles/CenterFrame.module.css';
 
 interface CenterFrameProps {
-  to: string;
+  to: string; // 추가: 클릭 시 이동할 URL 경로
 }
 
-function SUpCenterFrame({ to }: CenterFrameProps) {
+const SUpCenterFrame: React.FC<CenterFrameProps> = ({ to }) => {
   return (
     <div className={styles.centerFrame}>
       <p className={styles.topText}>Sign - Up</p>
-      <div className={styles.blackLine} />
+      <div className={styles.blackLine}></div>
       <Link to={to} className={styles.nonlineButton}>
         Sign In
       </Link>
     </div>
   );
-}
+};
 
 export default SUpCenterFrame;

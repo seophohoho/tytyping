44b9c30f769cc505e3fd79c61f8720c7131aa771
nahyeonkim0209 <miayoon0209@gmail.com>
2024-06-09 02:08:26 +0900

    [Design]: 사용자 구분

--- client/src/component/Game.tsx
+++ client/src/component/Game.tsx
@@ -5,7 +5,7 @@ import btn from '../styles/MatchingReady.module.css';
 import game from '../styles/Game.module.css';
 
 function Game(props: any) {
-  const { userInfo, targetUserInfo, socketInfo, setGameState, initGameInfo } = props;
+  const { userInfo, targetUserInfo, socketInfo, initGameInfo } = props;
 
   const [isTurn, setIsTurn] = useState(null);
   const [startWord, setStartWord] = useState('');
@@ -17,8 +17,10 @@ function Game(props: any) {
     setStartWord(initGameInfo.initWord);
     setIsTurn(initGameInfo.turn);
     socketInfo.on('ingame_target_input', (data: any) => {
-      //data에는 상대방이 작성한 단어와 올바른 단어인지 틀린 단어인지 판별하는 값이 있다.
+      // data에는 상대방이 작성한 단어와 올바른 단어인지 틀린 단어인지 판별하는 값이 있다.
+      console.log(data);
       setInputValue(data.input);
+      setIsCorrect(data.result);
     });
     socketInfo.on('ingame_change_response', (data: any) => {
       console.log('turn is over!');
@@ -38,15 +40,17 @@ function Game(props: any) {
         });
         if (res.data.message) {
           setIsCorrect(true);
-          socketInfo.emit('ingame_my_input', { input: inputValue, targetUserInfo: targetUserInfo, result: true });
-          //1.5초 뒤에 아래 코드가 실행되도록 해주면 된다.
-          socketInfo.emit('ingame_change_request', { targetUserInfo: targetUserInfo });
+          socketInfo.emit('ingame_my_input', { input: inputValue, targetUserInfo, result: true });
+          // 1.5초 뒤에 아래 코드가 실행되도록 해주면 된다.
+          socketInfo.emit('ingame_change_request', { targetUserInfo });
         } else {
           setIsCorrect(false);
-          socketInfo.emit('ingame_my_input', { input: inputValue, targetUserInfo: targetUserInfo, result: false });
+          socketInfo.emit('ingame_my_input', { input: inputValue, targetUserInfo, result: false });
         }
       } catch (error) {
         console.error('Error submitting word:', error);
+      } finally {
+        setInputValue('');
       }
     }
   };
@@ -57,17 +61,33 @@ function Game(props: any) {
         <div className={`${game.gameContainer}`}>
           <div className={`${btn.matchingBtn}`}>ROUND {round}</div>
           <div className={`${btn.matchingBtn} ${game.bigBox}`}>{startWord}</div>
-          <div className={`${btn.matchingBtn} ${game.bigBox}`}>
-            {isCorrect === true ? inputValue + '(맞음)' : inputValue + '(틀림)'}
+          <div className={`${btn.matchingBtn} ${game.bigBox}`}>${inputValue}</div>
+          <div className={game.wrongWord}>
+            {isCorrect ? `맞았습니다! 턴이 넘어갑니다.` : `없는 단어입니다. 다른 단어를 입력해주세요.`}
           </div>
           <div className={`${game.gameInputContainer}`}>
             <div className={`${game.gameInputUser}`}>
-              <div className={`${btn.matchingBtn}`} style={{ borderBottom: '0px' }}>
-                {userInfo.nickname}
-              </div>
-              <div className={`${btn.matchingBtn}`} style={{ borderBottom: '0px', borderLeft: '0px' }}>
-                {targetUserInfo.nickname}
-              </div>
+              {isTurn === 1 ? (
+                <div className={`${btn.matchingBtn}`} style={{ borderBottom: '0px' }}>
+                  {userInfo.nickname}
+                </div>
+              ) : (
+                <div className={`${btn.matchingBtn}`} style={{ borderBottom: '0px', background: '#929292' }}>
+                  {userInfo.nickname}
+                </div>
+              )}
+              {isTurn === 1 ? (
+                <div
+                  className={`${btn.matchingBtn}`}
+                  style={{ borderBottom: '0px', borderLeft: '0px', background: '#929292' }}
+                >
+                  {targetUserInfo.nickname}
+                </div>
+              ) : (
+                <div className={`${btn.matchingBtn}`} style={{ borderBottom: '0px', borderLeft: '0px' }}>
+                  {targetUserInfo.nickname}
+                </div>
+              )}
             </div>
             <div>
               <div className={`${btn.matchingBtn} ${game.inputContainer}`}>

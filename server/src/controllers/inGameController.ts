6e0960591f7e0557e2966a06aaa1906fb349c9c2
seophohoho <seop0937@gmail.com>
2024-06-09 03:47:50 +0900

    Fix error xml to json

--- server/src/controllers/inGameController.ts
+++ server/src/controllers/inGameController.ts
@@ -1,20 +1,24 @@
 import axios from "axios";
 import { API_KEY } from "../../key";
-import convert from "xml-js";
+import { parseString, parseStringPromise } from "xml2js";
 
 function makeUrl(word: string) {
   return `https://krdict.korean.go.kr/api/search?key=${API_KEY}&type_search=search&part=word&q=${word}&method=start&sort=dict&_csrf=a4e3be16-3af7-4b8e-8abe-ca6ddfa9130c`;
 }
 
-async function isCorrectWord(params: string) {
+async function isCorrectWord(params: string): Promise<any> {
   const word = params;
   try {
     const res = await axios.get(makeUrl(word));
-    const result = convert.xml2json(res.data, { compact: true, spaces: 4 });
-    return result;
-  } catch (error: any) {
+    const xmlData = res.data; // AxiosResponse 객체에서 data 추출
+    const jsonResult = await parseStringPromise(xmlData, {
+      explicitArray: false,
+    });
+
+    return JSON.stringify(jsonResult, null, 2);
+  } catch (error) {
+    console.error("Error fetching or parsing XML:", error);
     return null;
   }
 }
-
 export default isCorrectWord;
--- server/src/routes/inGameRoute.ts
+++ server/src/routes/inGameRoute.ts
@@ -1,32 +1,22 @@
 import express from "express";
 import isCorrectWord from "../controllers/inGameController";
-
 const router = express.Router();
 
 router.post("/is-correct", async (req: any, res: any) => {
   const { word } = req.body;
-  const result = await isCorrectWord(word)
-    .then((data: any) => {
-      if (JSON.parse(data).channel.total._text === "0") {
-        return [""];
-      } else {
-        const parse = JSON.parse(data).channel.item.map(
-          (item: any) => item.word._text
-        );
-        return parse;
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
-  if (word === result[0]) {
-    if (result[0].length === 1) {
-      return res.status(200).json({ message: false });
-    } else {
+  const result = await isCorrectWord(word);
+  const parse = JSON.parse(result);
+  // console.log(parse);
+  if (parse.channel.total === "0") {
+    return res.status(200).json({ message: false });
+  } else if (parse.channel.total === "1") {
+    if (parse.channel.item.word === word) {
       return res.status(200).json({ message: true });
     }
   } else {
-    return res.status(200).json({ message: false });
+    if (JSON.parse(result).channel.item[0].word === word) {
+      return res.status(200).json({ message: true });
+    }
   }
 });
 
